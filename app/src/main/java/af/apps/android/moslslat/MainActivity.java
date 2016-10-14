8f6dcf0b5ea0354c601356d3b3dc79a43ef3bbc2
asemalfaqeh <technicalasem@gmail.com>
2016-10-14 10:45:02 +0300

    New Update 14/10 10:45Am

--- app/src/main/java/af/apps/android/moslslat/MainActivity.java
+++ app/src/main/java/af/apps/android/moslslat/MainActivity.java
@@ -2,12 +2,24 @@ package af.apps.android.moslslat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import af.apps.android.moslslat.AdaptersClass.MainAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    public StaggeredGridLayoutManager staggeredGridLayoutManager;
+    public MainAdapter adapter;
+    public RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerview1);
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        adapter = new MainAdapter(this);
+        recyclerView.setAdapter(adapter);
     }
 }
